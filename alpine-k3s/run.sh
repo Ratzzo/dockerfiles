9b3fd080b4dc5c8fc87d31dc5d185c685f64ddb8
Ratzzo <ignacio.4096@gmail.com>
2022-11-14 03:53:36 -0600

    delete dalpine-k3s as it didn't work

--- alpine-k3s/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-mkdir -p $PWD/shared
-docker run --privileged -v $PWD/shared:/tmp/shared --env SSH_PORT --network host --rm -it ratzzo/alpine-k3s
--- alpine-k3s/ssh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-ssh -i $PWD/shared/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no updater-stub-user@127.0.0.1
